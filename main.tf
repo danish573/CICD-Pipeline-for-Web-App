36836cec80d01c0364a27bffa79252b82ec1d2d4
danish573 <dkhan573@gmail.com>
2025-08-08 18:30:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -80,6 +80,13 @@ resource "aws_security_group" "s_group" {
     cidr_blocks = [ "0.0.0.0/0" ]
   }
 
+  ingress {
+    from_port = 5000
+    to_port = 5000
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
   egress {
     from_port = 0
     to_port = 0
