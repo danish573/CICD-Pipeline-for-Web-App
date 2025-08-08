912de3fd32d774c9820e3845a815cba7e2c1481e
danish573 <dkhan573@gmail.com>
2025-08-08 18:31:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Welcome to the Flask App!, Hello from CI/CD pipeline with jenkins, Docker & EC-2"
+    return "Welcome to the Flask App!, Hello from CI/CD pipeline with jenkins, Docker, Githu & EC-2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
