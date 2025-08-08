1d97cabb8747fad0440fdd4f8d1882110caff5d4
danish573 <dkhan573@gmail.com>
2025-08-08 18:14:44 +0530

    Update docker-insallation.sh

--- docker-insallation.sh
+++ docker-insallation.sh
@@ -1,8 +1,12 @@
-#install docker on amazon linux 2
-#!/bin/bash
+# Update and install Docker
 sudo yum update -y
 sudo amazon-linux-extras install docker -y
+sudo yum install docker -y
+
+# Start and enable Docker
 sudo systemctl start docker
 sudo systemctl enable docker
 
+# Add ec2-user to docker group (optional, for non-root use)
+sudo usermod -aG docker ec2-user
 
